a8ba2e47d6014f0af7911c48f3124d870440afec
Bryan Stearns <bryanstearns@gmail.com>
2016-12-22 15:16:56 -0800

    Add server:true to prod endpoint; bump prod logging level

--- config/prod.exs
+++ config/prod.exs
@@ -14,10 +14,11 @@ use Mix.Config
 config :distillery_experiment, DistilleryExperiment.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
